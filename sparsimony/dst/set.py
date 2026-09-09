368f665457762c35300b927324480ed07a78e7ca
TejasPote <tejaspote915@gmail.com>
2026-09-09 11:54:58 -0600

    fix ci/cd integration

--- sparsimony/dst/set.py
+++ sparsimony/dst/set.py
@@ -73,9 +73,13 @@ class SET(DSTMixin, BaseSparsifier):
         else:
             weights = getattr(module, tensor_name)
             original_weights = get_original_tensor(module, tensor_name)
-            target_sparsity = self.get_sparsity_from_prune_ratio(mask, prune_ratio)
+            target_sparsity = self.get_sparsity_from_prune_ratio(
+                mask, prune_ratio
+            )
             self.prune_mask(target_sparsity, mask, values=weights)
-            self.grow_mask(sparsity, mask, original_weights, values = original_weights)
+            self.grow_mask(
+                sparsity, mask, original_weights, values=original_weights
+            )
             self._assert_sparsity_level(mask, sparsity)
 
     def _initialize_masks(self) -> None:
