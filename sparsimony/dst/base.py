2e879b9ae63d36433939d1ccbd66953ab5f53695
TejasPote <tejaspote915@gmail.com>
2026-09-09 11:23:51 -0600

    Fixed mask regrowth in base

--- sparsimony/dst/base.py
+++ sparsimony/dst/base.py
@@ -101,7 +101,7 @@ class DSTMixin(ABC):
         # Grow new weights
         new_mask = self.grower.calculate_mask(sparsity, mask, *args, **kwargs)
         # Assign newly grown weights to self.grown_weights_init
-        torch.where(
+        original_weights.data = torch.where(
             new_mask != old_mask,
             torch.full_like(
                 original_weights, fill_value=self.grown_weights_init
